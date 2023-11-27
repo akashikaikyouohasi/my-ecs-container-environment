9119fc5b80715e9360c51a38096ca89111ac5058
Yusuke Sakai <you@example.com>
2023-11-27 09:03:55 +0900

    条件変更

--- terrform/common/iam.tf
+++ terrform/common/iam.tf
@@ -15,11 +15,11 @@ data "aws_iam_policy_document" "github_actions_ecs_deploy_assume_role_policy" {
       type        = "Federated"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
     }
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
+    # condition {
+    #   test     = "StringEquals"
+    #   variable = "token.actions.githubusercontent.com:aud"
+    #   values   = ["sts.amazonaws.com"]
+    # }
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
